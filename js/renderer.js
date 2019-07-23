541121f8e4ef6cf4081c5f5aa1facae6688babba
Christopher Torres <7156279+RavenX8@users.noreply.github.com>
2019-07-23 00:34:29 -0400

    Stoof works!

--- js/renderer.js
+++ js/renderer.js
@@ -1,30 +1,40 @@
 const {ipcRenderer} = require('electron');
 const {log} = require('electron-log');
 
+document.getElementById("btn-launch").disabled = true;
+let processName = "";
+let processArgs = [];
+let serverIp = "";
+let serverPort = "";
+
+
+var serverString = null;
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function()
+{
+  if (this.status == 200)
+  {
+    serverString = xhttp.responseText;
+    var textByLine = serverString.split(":");
+
+    if(textByLine.length >= 2) {
+      serverIp = textByLine[0];
+      serverPort = textByLine[1];
+    }
+  }
+};
+xhttp.open("GET", "https://azgstudio.com/get_serverip.php", true);
+xhttp.send();
+
+
 function updateProgress(cProgress) {
   var elem = document.getElementById("myBar");
   elem.style.width = cProgress + '%';
-} 
+}
 
-ipcRenderer.on("download progress", (event, progress) => {
-  //const progressInPercentages = progress * 100; // With decimal point and a bunch of numbers
-  const cleanProgressInPercentages = Math.floor(progress * 100); // Without decimal point
-  if(cleanProgressInPercentages !== 0)
-    updateProgress(cleanProgressInPercentages);
-});
-
-ipcRenderer.on("download complete", (event, file) => {
-  if(file.includes(".zip") == true) {
-    ipcRenderer.send("extract file", {
-      file: file
-    });
-  } else if (file === "root.js") {
-    console.log('we finished downloading root.js');
-
-    // TODO: Do something with root.js
-  }
-  //console.log('we finished downloading ' + file);
-});
+function checkForUpdate(value) {
+  ipcRenderer.sendSync("check-file", value);
+}
 
 function download_update(url) {
   ipcRenderer.send("download", {
@@ -33,9 +43,45 @@ function download_update(url) {
   });
 }
 
-ipcRenderer.send("download", {
-  url: "https://azgstudio.com/patch/root.js",
-  properties: {directory: "."}
+function send_launch() {
+  for(var idx = 0; idx < processArgs.length; idx++)
+  {
+    if(processArgs[idx] == '$SERVER_IP') {
+      processArgs[idx] = serverIp;
+    } else if(processArgs[idx] == '$SERVER_PORT') {
+      processArgs[idx] = serverPort;
+    }
+  }
+
+  ipcRenderer.send('launch-game', {
+    processName: processName,
+    processArgs: processArgs
+  });
+}
+
+function update_complete() {
+  document.getElementById("btn-launch").disabled = false;
+}
+
+ipcRenderer.on("download progress", (event, progress) => {
+  const cleanProgressInPercentages = Math.floor(progress * 100); // Without decimal point
+  if(cleanProgressInPercentages !== 0)
+    updateProgress(cleanProgressInPercentages);
+});
+
+ipcRenderer.on("server address update", (event, info) => {
+  serverIp = info.ip;
+  serverPort = info.port;
+});
+
+ipcRenderer.on("download complete", (event, file) => {
+  if(file.includes(".zip") == true) {
+    console.log('sending \'extract file\' event for \'' + file + '\'');
+    ipcRenderer.send("extract file", {filename: file});
+  }
+  else {
+    console.log('we finished downloading ' + file);
+  }
 });
 
 // ipcRenderer.on('message', function(event, text) {
--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 const {app, BrowserWindow, ipcMain} = require('electron');
 const {download} = require("electron-dl");
-const {log} = require('electron-log');
 const {autoUpdater} = require("electron-updater");
-const {fs} = require('fs');
-const {path} = require('path');
+const fs = require('fs');
+const {spawn} = require('child_process');
 var DecompressZip = require('decompress-zip');
+var sha256 = require('js-sha256');
 
 // Make sure javascript doesn't kill our window before we are done with it
-let win
+let win;
 
 function createWindow () {
   // Create the browser window.
+  var basepath = app.getAppPath();
   let win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -23,10 +24,14 @@ function createWindow () {
     }
   })
   // and load the index.html of the app.
-  win.loadFile('html/index.html')
+  win.loadFile('html/index.html', {"extraHeaders" : "pragma: no-cache\n"})
+
+  win.webContents.session.clearCache(function(){
+    //some callback.
+  });
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  //win.webContents.openDevTools();
 
   win.once('ready-to-show', () => {
     win.show()
@@ -40,40 +45,85 @@ function createWindow () {
     win = null
   })
 
-  ipcMain.on("download", (event, info) => {
+  ipcMain.on("download", async (event, info) => {
     info.properties.onProgress = status => win.webContents.send("download progress", status);
     download(BrowserWindow.getFocusedWindow(), info.url, info.properties)
         .then(dl => win.webContents.send("download complete", dl.getSavePath()));
   });
 
   ipcMain.on("extract file", (event, info) => {
-    var unzipper = new DecompressZip(info.file);
+    // console.log('got extract file event for \'' + info.filename + '\'');
+    var unzipper = new DecompressZip(info.filename);
 
     unzipper.on('error', function (err) {
-      log.error('Caught an error');
+      // console.log('Caught an error');
+      // console.log(err);
+    });
+
+    unzipper.on('progress', function (fileIndex, fileCount) {
+      //console.log('Extracted file ' + (fileIndex + 1) + ' of ' + fileCount);
     });
 
     unzipper.on('extract', function (log) {
-      log.info(log);
-      // if (fs.existsSync(log.filename)) {
-      //   fs.unlink(log.filename, (err) => {
-      //       if (err) {
-      //           return;
-      //       }
-      //   });
-      // }
+      if (fs.existsSync(this.filename)) {
+        fs.unlink(this.filename, (err) => {if (err) {return;}});
+      }
     });
 
     unzipper.extract({
-      path: '.',
       filter: function (file) {
           return file.type !== "SymbolicLink";
       }
     });
   });
-}
 
-//let working_directory = app.getPath('app')
+  ipcMain.on("check-file", (event, info) => {
+    if (fs.existsSync(info.filename)) {
+      var file_for_update = fs.readFileSync(info.filename);
+      var hash = sha256(file_for_update.toString());
+      console.log("Hash: '" + hash.toUpperCase() + "'");
+      console.log("info.hash: '" + info.hash.toUpperCase() + "'");
+      if(hash.toUpperCase() != info.hash.toUpperCase())
+      {
+        download(BrowserWindow.getFocusedWindow(), info.url, {directory: "."})
+          .then(dl => {
+            win.webContents.send("download complete", dl.getSavePath())
+            event.returnValue = true;
+          });
+      }
+    } else {
+      //console.log(info.url);
+      //TODO: need to download file now
+      download(BrowserWindow.getFocusedWindow(), info.url, {directory: "."})
+        .then(dl => {
+          win.webContents.send("download complete", dl.getSavePath())
+          event.returnValue = true;
+        });
+    }
+  });
+
+  ipcMain.on("delete-file", (event, info) => {
+    if (fs.existsSync(info.filename)) {
+      fs.unlink(info.filename, (err) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+      });
+    }
+  });
+
+  ipcMain.on("launch-game", (event, info) => {
+    // console.log("launching game!!!!");
+    // console.log(info.processName);
+    // console.log(info.processArgs);
+    const subprocess = spawn(info.processName, info.processArgs, {
+      detached: true,
+      stdio: 'ignore'
+    });
+    subprocess.unref();
+  });
+}
 
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
@@ -81,7 +131,7 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
 
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
@@ -89,29 +139,28 @@ app.on('activate', () => {
   if (win === null) {
     createWindow()
   }
-})
+});
+
+app.on('ready', function()  {
+  //autoUpdater.checkForUpdates();
+  createWindow();
+});
 
 autoUpdater.on('error', message => {
-  log.error('There was a problem updating the application')
-  log.error(message)
-})
+  console.log('There was a problem updating the application')
+  console.log(message)
+});
 
 // If there aren't any updates, create the window
 autoUpdater.on('update-not-available', (ev, info) => {
   createWindow();
-})
+});
 
 autoUpdater.on('update-downloaded', (ev, info) => {
   // Wait 5 seconds, then quit and install
   // In your application, you don't need to wait 5 seconds.
   // You could call autoUpdater.quitAndInstall(); immediately
-  // setTimeout(function() {
-  //   autoUpdater.quitAndInstall();
-  // }, 5000)
-  autoUpdater.quitAndInstall();
-})
-
-app.on('ready', function()  {
-  //autoUpdater.checkForUpdates();
-  createWindow();
+  //setTimeout(function() {
+    autoUpdater.quitAndInstall();
+  //}, 1000);
 });
