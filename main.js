1a19d5fcb16df490ae429647dd48504b7511ece2
Raven <7156279+RavenX8@users.noreply.github.com>
2019-10-07 14:06:36 -0400

    Disabled web devtools

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ function createWindow () {
   win.loadFile('index.html', {"extraHeaders" : "pragma: no-cache\n"})
 
   // Open the DevTools.
-  if(devMode) win.webContents.openDevTools();
+  //if(devMode) win.webContents.openDevTools();
 
   win.webContents.on('will-navigate', (event, url) => {
     event.preventDefault();
